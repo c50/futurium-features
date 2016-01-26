af8c548ae3f2fd2004aa87fd50427fd150ca5e7c
Horacio Lopes <HoracioLopes@gmail.com>
2016-01-26 10:34:12 +0100

    Ckeditor placeholder plugin

--- themes/futurium_isa_theme/template.php
+++ themes/futurium_isa_theme/template.php
@@ -338,3 +338,25 @@ function futurium_isa_theme_menu_tree__menu_user_tabs($variables) {
 function futurium_isa_theme_menu_tree__menu_group_tabs($variables) {
   return '<ul class="menu nav nav-pills">' . $variables['tree'] . '</ul>';
 }
+
+
+function futurium_isa_theme_textarea($variables) {
+  $element = $variables['element'];
+  element_set_attributes($element, array('id', 'name', 'cols', 'rows'));
+  _form_set_class($element, array('form-textarea'));
+
+  $wrapper_attributes = array(
+    'class' => array('form-textarea-wrapper'),
+  );
+
+  // Add resizable behavior.
+  if (!empty($element['#resizable'])) {
+    drupal_add_library('system', 'drupal.textarea');
+    $wrapper_attributes['class'][] = 'resizable';
+  }
+
+  $output = '<div' . drupal_attributes($wrapper_attributes) . '>';
+  $output .= '<textarea placeholder="oh my darlin" ' . drupal_attributes($element['#attributes']) . '>' . check_plain($element['#value']) . '</textarea>';
+  $output .= '</div>';
+  return $output;
+}
