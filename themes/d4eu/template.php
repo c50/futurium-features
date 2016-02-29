f03b3aa973585d4c84793d55812a1ddd7df8030f
Eric Lemonne <eric@c50.be>
2016-02-29 13:45:36 +0100

    DAE-1412: improve condition to avoid undefined und notice

--- themes/d4eu/template.php
+++ themes/d4eu/template.php
@@ -242,13 +242,13 @@ function d4eu_html_head_alter(&$head_elements) {
  */
 function d4eu_preprocess_node(&$vars) {
   $account = user_load($vars['node']->uid);
-  if (isset($field_organisation)) {
+  if (isset($account->field_organisation[LANGUAGE_NONE][0]['safe_value'])) {
     $organisation = $account->field_organisation[LANGUAGE_NONE][0]['safe_value'];
   }
   $vars['submitted']  = '<div class="authoring-info">';
   $vars['submitted'] .= '<span class="published-by">' . t("Published by") . ' </span>';
   $vars['submitted'] .= '<span class="username">' . theme('username', array('account' => $account)) . '</span>';
-  if (isset($organisation) && ($organisation != '')) {
+  if (isset($organisation) && ($organisation != ' ')) {
     $vars['submitted'] .= '<span class="organisation">' . $organisation . '</span>';
   }
   $vars['submitted'] .= '<span class="extra-on"> ' . t("on") . ' </span> ';
