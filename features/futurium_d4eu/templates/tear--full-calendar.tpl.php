9fcffc3d4f47ba9ac01b5370742e8b4d7b11ada0
Eric Lemonne <eric@c50.be>
2016-05-13 17:21:58 +0200

    MULTISITE-1064: tear_off_full calendar in a template

---
+++ features/futurium_d4eu/templates/tear--full-calendar.tpl.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @file
+ * Custom theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all,
+ *   or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $display_user_picture: Whether node author's picture should be displayed.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct url of the current node.
+ * - $display_submitted: Whether submission information should be displayed.
+ * - $submitted: Submission information created from $name and $date during
+ *   template_preprocess_node().
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - node: The current template type, i.e., "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Blog entry" it would result in "node-blog". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
+ *     listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode, e.g. 'full', 'teaser'...
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined, e.g. $node->body becomes $body. When needing to access
+ * a field's raw values, developers/themers are strongly encouraged to use these
+ * variables. Otherwise they will have to explicitly specify the desired field
+ * language, e.g. $node->body['en'], thus overriding any language negotiation
+ * rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ */
+?>
+<?php if ($from_date): ?>
+<div class='fromTo-date'>
+  <a href="">
+  <div class='dateColour clearfix'>
+    <div class='day-event'><?php print($from_date[2]); ?></div>
+    <div class='month-event'><?php print($from_date[1]); ?></div>
+    <div class='year-event'><?php print($from_date[0]); ?></div>
+    <div class='week-day-event'><?php print($from_day); ?></div>
+  </div>
+  <?php if ($from_time): ?>
+    <div class='time-event'><span class='fromHour'><?php print($from_time) ?></span>
+    <?php if ($from_date == $to_date): ?>
+    <span class='toLabel'> <?php t('to') ?> </span><span class='toHour'><?php print($to_time) ?></span>
+    <?php endif ?>
+    </div>
+  <?php endif ?>
+<?php endif ?>
+
+<?php if ($to_date): ?>
+    <a href="">
+    <div class='dateColour clearfix'>
+      <div class='day-event'><?php print($to_date[2]) ?></div>
+      <div class='month-event'><?php print($to_date[1]) ?></div>
+      <div class='year-event'><?php print($to_date[0]) ?></div>
+      <div class='week-day-event'><?php print($to_day) ?></div>
+    </div>
+    <div class='time-event'><span class='fromHour'><?php print($to_time) ?></span>
+    </div>
+  <?php endif ?>
+  </a>
+</div>
